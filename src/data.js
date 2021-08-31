a781e297dae923592efe99c16e2c59586805cfb2
TimWeyel <tweyel@gmail.com>
2021-08-31 12:59:31 -0700

    added feed-side-suggestions, follow buttons, and loading screen icon

--- src/data.js
+++ src/data.js
@@ -4,10 +4,10 @@ export const defaultUser = {
   id: uuid(),
   username: "username",
   name: "name",
-  profile_image:
-    "https://scontent-ort2-2.cdninstagram.com/v/t51.2885-19/s150x150/82559664_3161307737426774_8687807477812559872_n.jpg?_nc_ht=scontent-ort2-2.cdninstagram.com&_nc_ohc=llb3VCRb-mkAX_XTEi3&oh=64f0323db646c01299e513e3337b83ff&oe=5E8620BF"
   // profile_image:
-  // "https://instagram.com/static/images/anonymousUser.jpg/23e7b3b2a737.jpg"
+  //   "https://scontent-ort2-2.cdninstagram.com/v/t51.2885-19/s150x150/82559664_3161307737426774_8687807477812559872_n.jpg?_nc_ht=scontent-ort2-2.cdninstagram.com&_nc_ohc=llb3VCRb-mkAX_XTEi3&oh=64f0323db646c01299e513e3337b83ff&oe=5E8620BF"
+  profile_image:
+  "https://miro.medium.com/fit/c/131/131/1*JyYin7G7aGwgD9zpYBZ12Q.png"
 };
 
 export function getDefaultUser() {
@@ -16,7 +16,7 @@ export function getDefaultUser() {
     username: "username",
     name: "name",
     profile_image:
-      "https://scontent-ort2-2.cdninstagram.com/v/t51.2885-19/s150x150/82559664_3161307737426774_8687807477812559872_n.jpg?_nc_ht=scontent-ort2-2.cdninstagram.com&_nc_ohc=llb3VCRb-mkAX_XTEi3&oh=64f0323db646c01299e513e3337b83ff&oe=5E8620BF"
+      "https://miro.medium.com/fit/c/131/131/1*JyYin7G7aGwgD9zpYBZ12Q.png"
   };
 }
 
--- src/pages/feed.js
+++ src/pages/feed.js
@@ -2,13 +2,19 @@ import React from "react";
 import { useFeedPageStyles } from "../styles";
 import Layout from "../components/shared/Layout";
 import UserCard from "../components/shared/UserCard";
+import LoadingScreen from "../components/shared/LoadingScreen";
 import FeedPost from "../components/feed/FeedPost";
 import FeedSideSuggestions from "../components/feed/FeedSideSuggestions";
 import { getDefaultPost } from '../data';
 import { Hidden } from "@material-ui/core";
+import { LoadingLargeIcon } from "../icons";
 
 function FeedPage() {
   const classes = useFeedPageStyles();
+  const [isEndOfFeed] = React.useState(false);
+
+  let loading = false;
+  if (loading) return <LoadingScreen />
 
   return (
     <Layout>
@@ -23,12 +29,13 @@ function FeedPage() {
         <Hidden smDown>
             <div className={classes.sidebarContainer}>
               <div className={classes.sidebarWrapper}>
-                <UserCard />
+                <UserCard avatarSize={50} />
                 <FeedSideSuggestions />
               </div>
             </div>
 
         </Hidden>
+        {!isEndOfFeed && <LoadingLargeIcon />}
       </div>
     </Layout>
   )
