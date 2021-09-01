e749dadc0d519517bd8f50d220d6d7b41c8183e9
TimWeyel <tweyel@gmail.com>
2021-09-01 14:51:11 -0700

    minor update

--- src/components/notification/NotificationList.js
+++ src/components/notification/NotificationList.js
@@ -1,31 +1,34 @@
 import React from "react";
 import { useNotificationListStyles } from "../../styles";
-import { defaultNotifications } from '../../data';
-import { Avatar, Grid, Typography } from '@material-ui/core';
-import { Link } from 'react-router-dom';
-import FollowButton from '../shared/FollowButton';
+import { defaultNotifications } from "../../data";
+import { Grid, Avatar, Typography } from "@material-ui/core";
+import { Link } from "react-router-dom";
+import FollowButton from "../shared/FollowButton";
 import useOutsideClick from "@rooks/use-outside-click";
 
 function NotificationList({ handleHideList }) {
-  const listContainerRef = React.useRef();
   const classes = useNotificationListStyles();
+  const listContainerRef = React.useRef();
   useOutsideClick(listContainerRef, handleHideList);
 
   return (
-    <Grid ref={useOutsideClick} className={classes.listContainer} container>
+    <Grid ref={listContainerRef} className={classes.listContainer} container>
       {defaultNotifications.map(notification => {
-        const isLike = notification.type === 'like';
-        const isFollow = notification.type === 'follow';
+        const isLike = notification.type === "like";
+        const isFollow = notification.type === "follow";
 
         return (
           <Grid key={notification.id} item className={classes.listItem}>
             <div className={classes.listItemWrapper}>
               <div className={classes.avatarWrapper}>
-                <Avatar src={notification.user.profile_image} alt="User avatar"/>
+                <Avatar
+                  src={notification.user.profile_image}
+                  alt="User avatar"
+                />
               </div>
               <div className={classes.nameWrapper}>
                 <Link to={`/${notification.user.username}`}>
-                  <Typography variant = "body1">
+                  <Typography variant="body1">
                     {notification.user.username}
                   </Typography>
                 </Link>
@@ -38,7 +41,7 @@ function NotificationList({ handleHideList }) {
                   {isFollow && `started following you. 5d`}
                 </Typography>
               </div>
-            </div> 
+            </div>
             <div>
               {isLike && (
                 <Link to={`/p/${notification.post.id}`}>
@@ -48,10 +51,10 @@ function NotificationList({ handleHideList }) {
               {isFollow && <FollowButton />}
             </div>
           </Grid>
-        )
+        );
       })}
     </Grid>
-  )
+  );
 }
 
 export default NotificationList;
