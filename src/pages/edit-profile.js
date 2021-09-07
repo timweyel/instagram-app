fc8143ae928f57ff042f723ec1bc6c00a498056d
TimWeyel <tweyel@gmail.com>
2021-09-07 14:57:04 -0700

    added drawer and menu options array

--- src/pages/edit-profile.js
+++ src/pages/edit-profile.js
@@ -1,10 +1,60 @@
+import { Drawer, Hidden, IconButton, List } from "@material-ui/core";
+import { Menu } from '@material-ui/icons'
 import React from "react";
+import Layout from "../components/shared/Layout";
 import { useEditProfilePageStyles } from "../styles";
 
 function EditProfilePage() {
-  useEditProfilePageStyles();
+  const classes = useEditProfilePageStyles();
+  const [showDrawer, setDrawer] = React.useState(false);
 
-  return <div>EditProfilePage</div>;
+  function handleToggleDrawer() {
+    setDrawer(prev => !prev);
+  }
+
+  const option = [
+    "Edit Profile",
+    "Change Password",
+    "Apps and Websites",
+    "Email and SMS",
+    "Push Notifications",
+    "Manage Contacts",
+    "Privacy and Security",
+    "Login Activity",
+    "Emails from Instagram"
+  ]
+
+  const drawer = (
+    <List>
+
+    </List>
+  )
+
+  return (
+    <Layout title="Edit Profile">
+      <section className={classes.section}>
+        <IconButton edge="start"
+          onClick={handleToggleDrawer}
+          className={classes.menuButton}
+        >
+          <Menu />
+        </IconButton>
+        <nav>
+          <Hidden smUp impelementation="css">
+            <Drawer
+              variant="temporary"
+              anchor="left"
+              open={showDrawer}
+              onClose={handleToggleDrawer}
+              classes={{ paperAnchorLeft: classes.temporaryDrawer }}
+            >
+
+            </Drawer>
+          </Hidden>
+        </nav>
+      </section>
+    </Layout>
+  )
 }
 
 export default EditProfilePage;
