4554da268453e1baff43786d7cf095937862e0d5
TimWeyel <tweyel@gmail.com>
2021-09-08 12:26:46 -0700

    added service-worker route to now.json

---
+++ build/precache-manifest.4688052981d30f41d3ce159584351700.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bfec532c171d58e2b2fd8aba0852092",
+    "url": "/index.html"
+  },
+  {
+    "revision": "360fdfbc1ad54a922673",
+    "url": "/static/css/2.c60feeff.chunk.css"
+  },
+  {
+    "revision": "360fdfbc1ad54a922673",
+    "url": "/static/js/2.21971492.chunk.js"
+  },
+  {
+    "revision": "e8ccc7cf4c855a56e3a7079a2b405db4",
+    "url": "/static/js/2.21971492.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8dd1cd5bfb26867477fa",
+    "url": "/static/js/3.9e1e5803.chunk.js"
+  },
+  {
+    "revision": "7f991860dcbc6068fef4",
+    "url": "/static/js/main.cf38693c.chunk.js"
+  },
+  {
+    "revision": "d19ac21ebba298ad77cf",
+    "url": "/static/js/runtime-main.14282e1c.js"
+  },
+  {
+    "revision": "cbb9b56a5cf892318fc8ab502f9d3ba7",
+    "url": "/static/media/facebook-icon-blue.cbb9b56a.svg"
+  },
+  {
+    "revision": "a9a13771e5dcd73931e0b61df2faac62",
+    "url": "/static/media/icon-sheet-2.a9a13771.png"
+  },
+  {
+    "revision": "3dfb71b4bb35ca8b35728b9475b7d861",
+    "url": "/static/media/icon-sheet.3dfb71b4.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
